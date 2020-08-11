cc5b862660e5b6b805384aecde07bf20b82e205f
jason <719224032@qq.com>
2020-08-11 17:02:52 +0800

    引入云效maven私有仓库

---
+++ dubbo/src/main/java/com/jason/demo/dubbo/DemoDubboApplication.java
@@ -0,0 +1,13 @@
+package com.jason.demo.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDubboApplication.class, args);
+    }
+
+}
---
+++ dubbo/src/test/java/com/jason/demo/dubbo/DemoDubboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.demo.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDubboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
